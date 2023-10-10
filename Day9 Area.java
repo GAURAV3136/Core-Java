6885b7b6ea1d6969d770d9a1bd20c18c44f05fd1
Gaurav Shiromani <106577088+GAURAV3136@users.noreply.github.com>
2023-10-10 10:49:19 +0530

    Add files via upload

---
+++ Day9 Area.java
@@ -0,0 +1,10 @@
+package com.day9_InterfaceWrapper.interfaces;
+
+//to achieve the abstraction by using abstract method
+//Interface
+public interface Area {
+	//abstract method
+	void circle();
+	void square();
+
+}
---
+++ Day9 Execute.java
@@ -0,0 +1,11 @@
+package com.day9_InterfaceWrapper.interfaces;
+
+public class Execute {
+
+	public static void main(String[] args) {
+		ShapeArea a=new ShapeArea();
+		a.circle();
+		a.square();
+	}
+
+}
